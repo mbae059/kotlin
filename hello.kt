6e7f98580be190a0127d236f4e3b3e19bbb9ee38
Minjun Bae <mbae059@daum.net>
2022-03-29 16:37:23 +0900

    learning kotlin

--- hello.kt
+++ hello.kt
@@ -1,3 +1,11 @@
 fun main() {
     println("Hello World")
+
+
+    val items : List<String> = listOf("apple", "banana", "Orange")
+    
+    for(idx in items.indices) {
+        println("index is $idx, value is ${items[idx]}")
+    }
+
 }
